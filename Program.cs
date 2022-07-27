8f4ceb267d47fa0afd00fabb16da36fe0fe293f0
9584546306 <anas9584546306@gmail.com>
2022-07-27 08:42:47 +0530

    mainmethod

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace FlipTheCoin
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int headCount = 0;
+            int tailcount = 0;
+
+            double headpercentage = 0;
+            double tailpercentage = 0;
+
+            Console.WriteLine("Please enter the number of Flips");
+            int filps = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < filps; i++)
+            {
+                Random obj = new Random();
+
+                int num = obj.Next(2);
+
+                Console.WriteLine("The random number from system   --- " + num);
+
+                if (num == 0)
+                {
+                    Console.WriteLine("Tail");
+                    tailcount = tailcount + 1;
+                }
+                else if (num == 1)
+                {
+                    Console.WriteLine("Head");
+                    headCount++;
+                }
+
+            }
+
+            headpercentage = (headCount * 100) / filps;
+
+            tailpercentage = (tailcount * 100) / filps;
+
+            Console.WriteLine("HeadPercentage ---- " + headpercentage);
+
+            Console.WriteLine("TailPercentage ---- " + tailpercentage);
+
+
+
+        }
+    }
+}
